17c308b5865f87c6ef9b62290db3e36cd235ff18
Maycon Alexsander <mayconalexsander@hotmail.com>
2023-05-14 19:42:29 -0300

    add animações e interação

--- desafio-19/assets/js/script.js
+++ desafio-19/assets/js/script.js
@@ -1,5 +1,28 @@
-const msg_box_value = document.querySelector(".message-box input").value
+const send_icon = document.querySelector(".send")
 
-if (msg_box_value !== "") {
-    
+function checkEmptyInput() {
+    const msg_box_value = document.querySelector(".message-box input").value
+
+    if (msg_box_value != "") {
+        send_icon.style.display = "flex"
+    } else {
+        send_icon.style.display = "none"
+    }
+}
+
+function sendMessage() {
+    const popup = document.querySelector(".pop-up")
+    popup.style.display = "initial"
+
+    document.querySelector(".pop-up span").innerText = "Mensagem enviada!"
+
+    setTimeout(function(){
+        popup.style.display = "none"
+    }, 3000);
+}
+
+function clearInput() {
+    document.querySelector(".message-box input").value = ""
+
+    checkEmptyInput()
 }
